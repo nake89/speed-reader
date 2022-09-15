07b7105edd5a5be1bac1718bc3e69c2eafbb0304
nake89 <nake89@gmail.com>
2022-09-15 11:26:19 +0300

    using flexbox

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
 console.log('start')
-//asdasd
-let text =
-  'When I do front-end dev, I no longer run a local instance. I have one script, that build, uploads and does cache invalidation. And it takes less than 10 seconds. No more allowing localhost for CORS (or even worse everything). Everything is running in the cloud as close to production because it is running on the same provider, just on a different stage (dev instead of production).'
 
 let el = document.getElementById('app')
 el.style.display = 'none'
-let startButton = document.getElementById('startButton')
-let textBoxContainer = document.getElementById('textBoxContainer')
+let startButton = document.getElementById('start-button')
+let textBoxContainer = document.getElementById('text-box-container')
 let i = 0
 let textArr
 startButton.addEventListener('click', () => {
-  let textBox = document.getElementById('textBox').value
+  let textBox = document.getElementById('text-box').value
   el.style.display = 'inline-block'
   textBoxContainer.style.display = 'none'
   textArr = textBox.split(' ')
   let speedReaderInterval = setInterval(() => {
     speedRead()
-  }, 400)
+  }, 100)
   if (i == textArr.length) {
     clearInterval(speedReaderInterval)
   }
