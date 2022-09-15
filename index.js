cae428b011525a3ec091219d2e8cfb01cee74179
nake89 <nake89@gmail.com>
2022-09-15 14:25:30 +0300

    added word count and how fast the page will be read

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ let el = document.getElementById('app')
 el.style.display = 'none'
 let startButton = document.getElementById('start-button')
 let textBoxContainer = document.getElementById('text-box-container')
+let textBox = document.getElementById('text-box')
+let wordCountDiv = document.getElementById('word-count')
 let i = 0
 let textArr
+getWordCount()
+
 startButton.addEventListener('click', () => {
-  let textBox = document.getElementById('text-box').value
+  let textBoxValue = textBox.value
   el.style.display = 'inline-block'
   textBoxContainer.style.display = 'none'
-  textArr = textBox.split(' ')
+  textArr = textBoxValue.split(' ')
   let speedReaderInterval = setInterval(() => {
     speedRead()
   }, 400)
@@ -18,6 +22,18 @@ startButton.addEventListener('click', () => {
     clearInterval(speedReaderInterval)
   }
 })
+
+textBox.addEventListener('input', () => {
+  getWordCount()
+})
+
+function getWordCount() {
+  let wordCount = textBox.value.split(' ').length
+  wordCountDiv.innerHTML = `Word count: ${wordCount} <br> WPM speed is: 300.<br>This text will read in ${(
+    wordCount / 5
+  ).toFixed()} seconds.`
+}
+
 function speedRead() {
   el.innerText = textArr[i] ?? ''
   i++
