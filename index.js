ca93fc449b52a4f5e197e44d97cd8cf5a762d8ed
Kevin Kivi <nake89@gmail.com>
2022-10-24 19:49:45 +0300

    added option to select WPM

--- index.js
+++ index.js
@@ -1,40 +1,50 @@
-console.log('start.')
+console.log("start.");
 
-let el = document.getElementById('app')
-el.style.display = 'none'
-let startButton = document.getElementById('start-button')
-let textBoxContainer = document.getElementById('text-box-container')
-let textBox = document.getElementById('text-box')
-let wordCountDiv = document.getElementById('word-count')
-let i = 0
-let textArr
-getWordCount()
+let WPM = 300;
+let debugDiv = document.getElementById("debug");
+//debugDiv.innerText = interval;
+let el = document.getElementById("app");
+el.style.display = "none";
+let startButton = document.getElementById("start-button");
+let textBoxContainer = document.getElementById("text-box-container");
+let textBox = document.getElementById("text-box");
+let wordCountDiv = document.getElementById("word-count");
+let i = 0;
+let textArr;
+getWordCount();
 
-startButton.addEventListener('click', () => {
-  let textBoxValue = textBox.value
-  el.style.display = 'inline-block'
-  textBoxContainer.style.display = 'none'
-  textArr = textBoxValue.split(' ')
+let wpmDropdown = document.getElementById("wpm");
+wpmDropdown.onchange = function () {
+  WPM = Number(this.value);
+  document.getElementById("wpmNum").innerText = this.value;
+};
+
+startButton.addEventListener("click", () => {
+  let textBoxValue = textBox.value;
+  el.style.display = "inline-block";
+  textBoxContainer.style.display = "none";
+  textArr = textBoxValue.split(" ");
+  let interval = (1 / (WPM / 60)) * 1000;
   let speedReaderInterval = setInterval(() => {
-    speedRead()
-  }, 400)
+    speedRead();
+  }, interval);
   if (i == textArr.length) {
-    clearInterval(speedReaderInterval)
+    clearInterval(speedReaderInterval);
   }
-})
+});
 
-textBox.addEventListener('input', () => {
-  getWordCount()
-})
+textBox.addEventListener("input", () => {
+  getWordCount();
+});
 
 function getWordCount() {
-  let wordCount = textBox.value.split(' ').length
-  wordCountDiv.innerHTML = `Word count: ${wordCount} <br> WPM speed is: 300.<br>This text will read in ${(
+  let wordCount = textBox.value.split(" ").length;
+  wordCountDiv.innerHTML = `Word count: ${wordCount} <br> WPM speed is: <span id="wpmNum">300</span>.<br>This text will read in ${(
     wordCount / 5
-  ).toFixed()} seconds.`
+  ).toFixed()} seconds.`;
 }
 
 function speedRead() {
-  el.innerText = textArr[i] ?? ''
-  i++
+  el.innerText = textArr[i] ?? "";
+  i++;
 }
